1f54c7b27e2fe9735b426a1d2b09ab240e39e248
unknown <mouni.shalaby@gmail.com>
2023-03-15 11:21:16 +0200

    add window.location to refresh

--- client/src/components/InputTOdo.js
+++ client/src/components/InputTOdo.js
@@ -2,17 +2,17 @@ import React, { Fragment, useState } from "react";
 
 const InputTOdo = () => {
   const [description, setDescription] = useState("");
-  
+
   const onSubmitForm = async (e) => {
     e.preventDefault();
     try {
       const body = { description };
-      const response=await fetch('http://localhost:5000/todos',{
-        method:"POST",
-        headers:{'Content-Type':"application/json"},
-        body:JSON.stringify(body)
-      })
-      console.log(response);
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      window.location = "/";
     } catch (error) {
       console.error(error.message);
     }
